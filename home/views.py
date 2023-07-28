e6a6b97ec4b0a20d35ea584606a06badfb8ee282
Luca Noah Gfeller <luca.gfeller2@gmail.com>
2023-07-28 17:01:07 +0200

    in home apps index view import and render Product model & in index.html replace existing code with webflow code, in products apps html template also replace existing code with webflow code

--- home/views.py
+++ home/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 
 def index(request):
     """A view to return home page"""
+    products = Product.objects.all()[:3]
 
-    return render(request, "home/index.html")
+    context = {
+    "products": products,
+    }
+
+    return render(request, "home/index.html", context)
 
 
 def handle_404_page(request, exception):
