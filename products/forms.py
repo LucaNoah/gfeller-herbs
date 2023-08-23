91af15c05816c882513783417de56b4c846fb44b
Luca Noah Gfeller <luca.gfeller2@gmail.com>
2023-08-24 00:22:23 +0200

    in products app add_review.html implement webflow design & in forms.py add function to add classes to form field, in cehckout apps forms.py files edit class name of added classes

--- products/forms.py
+++ products/forms.py
@@ -22,3 +22,11 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = ProductReview
         exclude = ("approved",)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add classes to all fields
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "form-input"
