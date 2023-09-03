fc3c9791e2c6e41246b37e36db10fe54424be295
LucaNoah <luca.gfeller2@gmail.com>
2023-09-03 16:38:11 +0200

    in settings.py add new heroku app url to ALLOWED HOSTS

--- gfeller_herbs/settings.py
+++ gfeller_herbs/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ["https://gfeller-herbs-c476c3239d66.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["https://gfeller-herbs-d2af5e64f88d.herokuapp.com/", "localhost"]
 
 # Application definition
 
